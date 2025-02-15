64d35163054affbf1460caff56facabd8bf6d466
Nandinishar3117 <nandinishar1806@gmail.com>
2025-02-15 17:59:26 +0530

    Solution to Find Maximum of Three Numbers

---
+++ Find Maximum of Three Numbers@@6788dd63490637cfc44e1b94/code.c
@@ -0,0 +1,23 @@
+int main(){
+    int a;
+    int b;
+    int c;
+
+    
+    scanf("%d",&a);
+    
+    scanf("%d",&b);
+    
+    scanf("%d",&c);
+   
+        if ((a>c )&& (a>b)){
+        printf("%d",a);}
+        else if(b>c && b>a) {
+            printf("%d",b);
+        }
+   
+     else if(c>a && c>b) {
+            printf("%d",c);
+        }
+  
+}
